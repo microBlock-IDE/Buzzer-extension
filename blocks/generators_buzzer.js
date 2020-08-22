3ade455f43a0dfc1b283fa86deeaa36d16142d02
Max <max30012540@hotmail.com>
2020-08-22 20:35:33 +0700

    Update generators_buzzer.js

--- blocks/generators_buzzer.js
+++ blocks/generators_buzzer.js
@@ -5,7 +5,7 @@ Blockly.Python['tone1'] = function(block) {
   var value_pin = Blockly.Python.valueToCode(block, 'pin', Blockly.Python.ORDER_ATOMIC);
   var value_freq = Blockly.Python.valueToCode(block, 'freq', Blockly.Python.ORDER_ATOMIC);
   var value_time = Blockly.Python.valueToCode(block, 'time', Blockly.Python.ORDER_ATOMIC);
-  var code = `PWM(Pin(${value_pin}), freq=${value_freq}, duty=50); sleep(${value_time}); Pin(${value_pin}, Pin.IN)\n`;
+  var code = `PWM(Pin(${value_pin}), freq=${value_freq}, duty=50); sleep(${value_time}); PWM(Pin(${value_pin}), freq=${value_freq}, duty=0)\n`;
   return code;
 };
 
@@ -24,6 +24,6 @@ Blockly.Python['no_tone'] = function(block) {
   Blockly.Python.definitions_['from_machine_import_pwm'] = 'from machine import PWM';
 
   var value_pin = Blockly.Python.valueToCode(block, 'pin', Blockly.Python.ORDER_ATOMIC);
-  var code = `Pin(${value_pin}, Pin.IN)\n`;
+  var code = `PWM(Pin(${value_pin}), freq=1000, duty=0)\n`;
   return code;
 };
