507f2c2f0a041b8972769c3dcb44fa30da02674d
Max <sonthaya.nt@hotmail.com>
2021-12-08 20:07:14 +0700

    Add RP2 chip support

--- blocks/generators_buzzer.js
+++ blocks/generators_buzzer.js
@@ -1,11 +1,25 @@
+function buzzerWriteFunction() {
+  return Blockly.Python.provideFunction_(
+    'buzzerWrite',
+    ['def ' + Blockly.Python.FUNCTION_NAME_PLACEHOLDER_ + '(pin, freq=1000, duty=50, stop=0):',
+    '  pwm = PWM(Pin(pin))',
+    '  pwm.freq(freq)',
+    '  pwm.duty_u16(int(duty / 100 * 65535))',
+    '  if stop > 0:',
+    '    sleep(stop)',
+    '    pwm.duty_u16(0)'
+  ]);
+}
+
 Blockly.Python['tone1'] = function(block) {
+  Blockly.Python.definitions_['from_time_import_sleep'] = 'from time import sleep';
   Blockly.Python.definitions_['from_machine_import_pin'] = 'from machine import Pin';
   Blockly.Python.definitions_['from_machine_import_pwm'] = 'from machine import PWM';
 
   var value_pin = Blockly.Python.valueToCode(block, 'pin', Blockly.Python.ORDER_ATOMIC);
   var value_freq = Blockly.Python.valueToCode(block, 'freq', Blockly.Python.ORDER_ATOMIC);
   var value_time = Blockly.Python.valueToCode(block, 'time', Blockly.Python.ORDER_ATOMIC);
-  var code = `PWM(Pin(${value_pin}), freq=${value_freq}, duty=50); sleep(${value_time}); PWM(Pin(${value_pin}), freq=${value_freq}, duty=0)\n`;
+  var code = `${buzzerWriteFunction()}(${value_pin}, freq=${value_freq}, stop=${value_time})\n`;
   return code;
 };
 
@@ -15,7 +29,7 @@ Blockly.Python['tone2'] = function(block) {
 
   var value_pin = Blockly.Python.valueToCode(block, 'pin', Blockly.Python.ORDER_ATOMIC);
   var value_freq = Blockly.Python.valueToCode(block, 'freq', Blockly.Python.ORDER_ATOMIC);
-  var code = `PWM(Pin(${value_pin}), freq=${value_freq}, duty=50)\n`;
+  var code = `${buzzerWriteFunction()}(${value_pin}, freq=${value_freq})\n`;
   return code;
 };
 
@@ -24,6 +38,6 @@ Blockly.Python['no_tone'] = function(block) {
   Blockly.Python.definitions_['from_machine_import_pwm'] = 'from machine import PWM';
 
   var value_pin = Blockly.Python.valueToCode(block, 'pin', Blockly.Python.ORDER_ATOMIC);
-  var code = `PWM(Pin(${value_pin}), freq=1000, duty=0)\n`;
+  var code = `${buzzerWriteFunction()}(${value_pin}, duty=0)\n`;
   return code;
 };
